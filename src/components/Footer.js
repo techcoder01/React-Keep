4b5deabcc836bbaa64550e9faa4bb02b456968be
techcoder01 <techcoderhelp@gmail.com>
2023-03-18 09:02:56 +0500

    React keep

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
+import { Paper } from '@material-ui/core'
 import React from 'react'
 
 function Footer() {
   return (
     <div>
-      <p style={{position:"fixed" , bottom: "0", width: '100%' , textAlign: 'center'}}>Copyright &copy; {new Date().getFullYear()} Abdul Hannan</p>
+    <Paper style={{marginTop: '20px'}} square elevation = {3}>
+      <p style={{position:"absolute" , bottom: "0", width: '100%' , textAlign: 'center'}}>Copyright &copy; {new Date().getFullYear()} Abdul Hannan</p>
+      </Paper>
     </div>
   )
 }
