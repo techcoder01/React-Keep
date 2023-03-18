d3cd0f69dc03e0a9db613765b1627e016ffc8471
techcoder01 <techcoderhelp@gmail.com>
2023-03-18 08:56:23 +0500

    React keep

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-      <p style={{position:"absolute" , bottom: "0", width: '100%' , textAlign: 'center'}}>Copyright &copy; {new Date().getFullYear()} Abdul Hannan</p>
+      <p style={{position:"fixed" , bottom: "0", width: '100%' , textAlign: 'center'}}>Copyright &copy; {new Date().getFullYear()} Abdul Hannan</p>
     </div>
   )
 }
